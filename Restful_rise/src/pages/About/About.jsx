e81bc304272db62735823fa4cb6dc02d70a63b42
Xurshida65 <130227441+Xurshida65@users.noreply.github.com>
2024-06-18 02:43:40 +0500

    Add files via upload

--- Restful_rise/src/pages/About/About.jsx
+++ Restful_rise/src/pages/About/About.jsx
@@ -1,5 +1,6 @@
 import "./about.css"
 import aboutImage from "../../assets/img/aboutImage.jpg"
+import { aboutDetails } from "../mock/aboutDetails";
 
 export const About = () => {
   return (
@@ -8,40 +9,12 @@ export const About = () => {
         <div className="container">
           <div className="about_content">
             <div className="about_leftSide">
-              <h1 className="about_title">Buxonjuujujro tabiiy mahsuloti</h1>
+              <h1 className="about_title">Buxoro tabiiy mahsuloti</h1>
               <div className="about_paragraph">
-                <p className="about_text">
-                  1. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko sumkalar,
-                  Fabrikamiz koʻp yillar davomida butun dunyoda foydalanish
-                  uchun paxta matolarini ishlab chiqaradigan kompaniya boʻlib
-                  kelgan.
-                </p>
-                <p className="about_text">
-                  2. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-                  sumkalar, ushbu kompaniya 17 yildan beri mijozlarga xizmat
-                  ko‘rsatmoqda.
-                </p>
-                <p className="about_text">
-                  3. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-                  sumkalar, ushbu kompaniyaning asosiy maqsadi yuqori sifatli va
-                  tejamkor mahsulotlar ishlab chiqarishdir.
-                </p>
-                <p className="about_text">
-                  4. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-                  sumkalar, ushbu kompaniyaning ishlab chiqarish jarayoni butunlay
-                  ekologik toza.
-                </p>
-                <p className="about_text">
-                  5. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-                  sumkalar, korxonada xaridorlarning talab va takliflari asosida
-                  har qanday turdagi paxta xomashyosi ishlab chiqarilishi
-                  mumkin.
-                </p>
-                <p className="about_text">
-                  6. Buxoro matolari, Eko sumkalar, Buxoro matolari, Eko
-                  sumkalar, kompaniya DongJia to‘quv mashinalarining
-                  O‘zbekistondagi rasmiy dileri hisoblanadi.
-                </p>
+                {aboutDetails.map((el) => <>
+                  <p className="about_text">{el.aboutText}</p>
+                </>
+                )}
               </div>
             </div>
             <div className="about_rightSide">
